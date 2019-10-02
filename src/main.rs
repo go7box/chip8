19109394336818500f2a1e4b621c05fbb02d2bd5
Manish Gill <manish.gill@fromatob.com>
2019-10-02 03:13:02 +0200

    Remove unnecessary thread

--- src/main.rs
+++ src/main.rs
@@ -16,29 +16,6 @@ mod opcodes;
 mod opcodesv2;
 mod ophandlers;
 
-/**
- * Start the machine in a separate thread.
- * We do this because we need to be able to parse instructions in one
- * thread and render the output in another. Otherwise we will block on
- * each instruction while doing the rendering.
-*/
-pub fn launch_thread<T>(
-    mut machine: core::Machine<T>,
-) -> JoinHandle<std::result::Result<(), String>>
-where
-    T: InstructionParser,
-    T: std::marker::Send,
-    T: 'static,
-{
-    std::thread::spawn(move || {
-        debug!(
-            "Inside the spawned thread: {:?}",
-            std::thread::current().id()
-        );
-        machine.start()
-    })
-}
-
 fn main() {
     env_logger::init();
     let rom_file = env::args().nth(1).expect("Please input a ROM file");
@@ -47,9 +24,5 @@ fn main() {
     vm.load_rom(&rom_file)
         .expect("Unable to load ROM from file");
     debug!("{:#?}", vm);
-    let handle = launch_thread(vm);
-    match handle.join() {
-        Ok(_) => info!("Shutting down..."),
-        Err(e) => error!("VM thread exited with error {:?}", e),
-    }
+    vm.start();
 }
